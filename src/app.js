2e7f56ed06611af9cf4087e265406a530aa84f1f
Time6628 <pturchan1@gmail.com>
2018-10-13 21:09:45 -0400

    cleanup, fix song skipping

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@
 const Eris = require("eris");
 const ytdl = require("ytdl-core");
 const fs = require("fs");
-const logerror = require("./logerror");
 
 if (!fs.existsSync("token.json")) {
     fs.writeFileSync("token.json", JSON.stringify({
@@ -29,23 +28,26 @@ bot.on('ready', function () {
 
 bot.registerCommand("play", (msg, args) => {
     if (msg.member.voiceState.channelID === null) {
-        msg.channel.createMessage("Please enter a voice channel.").catch(logerror.logerror);
+        msg.channel.createMessage("Please enter a voice channel.").catch(console.error);
+        return;
+    }
+    if (args[1] !== undefined && args[1] * .01 > 2.0 && msg.author.id !== "71371505652477952") {
+        msg.channel.createMessage("Can't set the volume that high.").catch(console.error);
+        return;
+    } else if (args[1] !== undefined && isNaN(parseInt(args[1], 10))) {
+        msg.channel.createMessage("That's not a valid volume.").catch(console.error);
         return;
     }
     bot.joinVoiceChannel(msg.member.voiceState.channelID).then((connection) => {
         if (connection.playing) {
             bot.createMessage(msg.channel.id, "Added " + args[0] + " to the queue.").then(() => {
                 queue.push(new Song(args[0], args[1]));
-            }).catch(logerror.logerror);
+            }).catch(console.error);
         } else {
-            if (args[1] !== undefined && args[1] * .01 > 2.0) {
-                msg.channel.createMessage("Can't set the volume that high.").catch(logerror.logerror);
-                return;
-            }
             console.log("Playing " + args[0] + " at " + args[1] + " volume.");
             players[connection] = new Player(connection, args[0], msg.channel, args[1]);
         }
-    }, logerror.logerror);
+    }, console.error);
 }, {
     description: "Play a song",
     usage: "<youtube url>"
@@ -53,31 +55,32 @@ bot.registerCommand("play", (msg, args) => {
 
 bot.registerCommand("skip", (msg) => {
     if (msg.member.voiceState.channelID === null) {
-        msg.channel.createMessage("Please join the voice channel first.").catch(logerror.logerror);
+        msg.channel.createMessage("Please join the voice channel first.").catch(console.error);
         return;
     }
     bot.joinVoiceChannel(msg.member.voiceState.channelID).then((connection) => {
-        msg.channel.createMessage("Skipping...").catch(logerror.logerror).then(() => {
+        msg.channel.createMessage("Skipping...").catch(console.error).then(() => {
             players[connection].skip();
         });
-    }, logerror.logerror);
+    }, console.error);
 }, {
     guildOnly: true
 });
 
 bot.registerCommand("volume", (msg, args) => {
+    if (args[0] !== undefined && isNaN(parseInt(args[0], 10))) return;
     if (msg.member.voiceState.channelID === null) {
-        msg.channel.createMessage("Please join the voice channel first.").catch(logerror.logerror);
+        msg.channel.createMessage("Please join the voice channel first.").catch(console.error);
         return;
     }
     bot.joinVoiceChannel(msg.member.voiceState.channelID).then((connection) => {
-        if (args[0] * .01 > 2.0) {
-            msg.channel.createMessage("Can't set the volume that high.").catch(logerror.logerror);
+        if (args[0] * .01 > 2.0 && msg.author.id !== "71371505652477952") {
+            msg.channel.createMessage("Can't set the volume that high.").catch(console.error);
             return;
         }
         connection.setVolume(args[0] * .01);
-        msg.channel.createMessage("Volume set to " + args[0] * .01).catch(logerror.logerror);
-    }, logerror.logerror);
+        msg.channel.createMessage("Volume set to " + args[0] * .01).catch(console.error);
+    }, console.error);
 }, {
     aliases: ["vol"],
     argsRequired: true,
@@ -86,18 +89,18 @@ bot.registerCommand("volume", (msg, args) => {
 
 bot.registerCommand("pause", msg => {
     if (msg.member.voiceState.channelID === null) {
-        msg.channel.createMessage("Please join the voice channel first.").catch(logerror.logerror);
+        msg.channel.createMessage("Please join the voice channel first.").catch(console.error);
         return;
     }
     bot.joinVoiceChannel(msg.member.voiceState.channelID).then((connection) => {
         if (!connection.paused) {
-            msg.channel.createMessage("Pausing playing.").catch(logerror.logerror);
+            msg.channel.createMessage("Pausing playing.").catch(console.error);
             connection.pause();
         } else if (connection.paused) {
-            msg.channel.createMessage("Resuming.").catch(logerror.logerror);
+            msg.channel.createMessage("Resuming.").catch(console.error);
             connection.resume();
         }
-    }, logerror.logerror);
+    }, console.error);
 }, {
     argsRequired: true,
     guildOnly: true
@@ -105,14 +108,14 @@ bot.registerCommand("pause", msg => {
 
 bot.registerCommand("queue", msg => {
     if (queue.length === 0) {
-        msg.channel.createMessage("Queue is empty.").catch(logerror.logerror);
+        msg.channel.createMessage("Queue is empty.").catch(console.error);
         return;
     }
     let content = "";
     queue.forEach((item) => {
         content += "<" + item.url + ">\n";
     });
-    msg.channel.createMessage(content).catch(logerror.logerror);
+    msg.channel.createMessage(content).catch(console.error);
 }, {
     argsRequired: true,
     guildOnly: true
@@ -125,17 +128,18 @@ function Player(connection, url, channel, volume) {
 
     this.playStream = function () {
         if (this.song.stream === null || undefined) {
-            bot.createMessage(this.channel_id, "Unable to play video.").catch(logerror.logerror);
+            bot.createMessage(this.channel_id, "Unable to play video.").catch(console.error);
             this.nextInQueue();
             return;
         }
-        this.connection.on("error", logerror.logerror);
-        this.connection.on("warn", logerror.logerror);
+        this.connection.on("error", console.error);
+        this.connection.on("warn", console.error);
+        //this.connection.on("debug", console.debug);
         this.connection.play(this.song.stream, {
             inlineVolume: true
         });
         this.connection.setVolume(this.song.volume);
-        this.channel.createMessage("Now playing: " + this.song.url).catch(logerror.logerror);
+        this.channel.createMessage("Now playing: " + this.song.url).catch(console.error);
         this.connection.once("end", () => {
             console.log("finished playing " + url);
             this.nextInQueue();
@@ -144,7 +148,6 @@ function Player(connection, url, channel, volume) {
 
     this.skip = function () {
         this.connection.stopPlaying();
-        this.nextInQueue();
     };
 
     this.nextInQueue = function () {
@@ -167,7 +170,7 @@ function Player(connection, url, channel, volume) {
 
 function Song(url, volume) {
     this.url = url;
-    this.stream = ytdl(url, {filter: "audioonly"}).on("error", logerror.logerror);
+    this.stream = ytdl(url, {filter: "audioonly"}).on("error", console.error);
     this.volume = isNaN(volume) ? 1.0 : volume * .01;
 }
 
